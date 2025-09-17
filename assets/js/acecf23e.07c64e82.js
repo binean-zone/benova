0ab7d861bfcfc1fe4b53f8d791c6704064d927c3
Tran Thien Nhan <tranthiennhan1984@gmail.com>
2025-09-17 18:16:56 +0700

    Deploy website - based on 7fed3ced31f963f2c386f4632ef4672b57dad8e4

---
+++ assets/js/acecf23e.07c64e82.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkbenova=self.webpackChunkbenova||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/benova/blog","blogTitle":"Blog","authorsListPath":"/benova/blog/authors"}')}}]);
---
+++ assets/js/bda4c7e3.1439a7c0.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkbenova=self.webpackChunkbenova||[]).push([[1876],{8158:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/benova/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
