ccc2031da484b9c1cdeacdd97d1b7388ca00b073
Tran Thien Nhan <tranthiennhan1984@gmail.com>
2025-09-17 17:46:19 +0700

    + update organizationName, url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,14 +15,14 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://binean-zone.github.io',
+  url: 'https://tranthiennhan1984.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/benova/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'binean', // Usually your GitHub org/user name.
+  organizationName: 'binean-zone', // Usually your GitHub org/user name.
   projectName: 'benova', // Usually your repo name.
 
   onBrokenLinks: 'throw',
